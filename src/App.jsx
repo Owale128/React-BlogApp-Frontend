9efb0f98c60794ffadac61a5030112aacf1174d3
Owale128 <Christian.buaka@hotmail.com>
2024-03-26 00:01:00 +0100

    Added the functions in return and also styled the buttons

--- src/App.jsx
+++ src/App.jsx
@@ -101,40 +101,73 @@ const BlogPosts = () => {
     }
   };
 
-  return (
-    <div className='wrapper'>
-      <h1>Blog Posts</h1>
-      <form onSubmit={handleSubmit}>
-      <div className='formContainer'>
-        <input
-        className='title'
-          type="text"
-          name="title"
-          placeholder="Enter title"
-          value={newPost.title}
-          onChange={handleInputChange}
-        />
-        <br></br>
-        <textarea
-        className='textArea'
-          name="content"
-          placeholder="Enter content"
-          value={newPost.content}
-          onChange={handleInputChange}
-        ></textarea>
-        <button type="submit" className='addBtn'>Add Post</button>
-        </div>
-        <br></br>
-      </form>
-      {blogPosts.map((post, i ) => (
-        <div key={i} className='wrapper2'>
-          <span className='deleteBtn' onClick={() => handleDelete(post._id)}>X</span>
-          <h4 className='postTitle'>{post.title}</h4>
-          <p className='postContent'>{post.content}</p>
-        </div>
-      ))}
-    </div>
-  );
+return (
+  <div className='wrapper'>
+    <h1>Blog Posts</h1>
+    <form onSubmit={handleSubmit}>
+    <div className='formContainer'>
+      <input
+      className='title'
+        type="text"
+        name="title"
+        placeholder="Enter title"
+        value={newPost.title}
+        onChange={handleInputChange}
+      />
+      <br></br>
+      <textarea
+      className='textArea'
+        name="content"
+        placeholder="Enter content"
+        value={newPost.content}
+        onChange={handleInputChange}
+      ></textarea>
+      <button type="submit" className='addBtn'>Add Post</button>
+      </div>
+      <br></br>
+    </form>
+    {blogPosts.map((post, i ) => (
+      <div key={i} className='wrapper2'>
+
+<span className='deleteBtn' onClick={() => handleDelete(post._id)}>Delete</span>
+        {editingPostId === post._id ? (
+          <>
+            <span className='updateBtn' onClick={() => handleUpdate(post._id)}>Update</span>
+          </>
+        ) : (
+          <span className='editBtn' onClick={() => handleEdit(post._id, post.title, post.content)}>Edit</span>
+          )}
+        {editingPostId === post._id ? (
+          <>
+            <input
+              className='title'
+              type="text"
+              name="title"
+              placeholder="Enter title"
+              value={editedPost.title}
+              onChange={(e) => setEditedPost({ ...editedPost, title: e.target.value })}
+            />
+            <br></br>
+            <textarea
+              className='textArea'
+              name="content"
+              placeholder="Enter content"
+              value={editedPost.content}
+              onChange={(e) => setEditedPost({ ...editedPost, content: e.target.value })}
+            ></textarea>
+            <button className='backBtn' onClick={handleCancelEdit}>Back</button>
+          </>
+        ) : (
+          <>
+            <h3 className='postTitle'>{post.title}</h3>
+            <p className='postContent'>{post.content}</p>
+          </>
+        )}
+      </div>
+
+    ))}
+  </div>
+);
 };
 
 export default BlogPosts;
